7daa25340d34075a263e918a0604a2f3a5b2c5da
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2022-10-30 09:46:13 +0000

    first addition of javascript

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+
+for(var x = 0; x < topics.length; x++){
+    console.log(topics[x])
+}
+
+/*
+if (topics[x] === 'HTML'){
+    console.log("Let's study HTML!")
+} else if (topics[x] === 'CSS'){
+    console.log("Let's study CSS!")
+} else if (topics[x] === 'Git'){
+    console.log("Let's study Git!")
+} else if (topics[x] === 'Javascript'){
+    console.log("Let's study Javascript!")
+} else {
+    console.log("Please try again!")
+}
+*/
+// var shapes = ["triangle", "square", "pentagon", "circle"];
+
+// console.log(shapes[0])
+
+// for(var x = 0; x < shapes.length; x++){
+//     console.log(shapes[x])
+// }
